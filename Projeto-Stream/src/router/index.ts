758963bb01a535926766f2a5d96545575536feb2
LindemirHoffmann <lindemirhoffmann10@gmail.com>
2024-06-27 16:23:13 -0300

    create view filmes

--- Projeto-Stream/src/router/index.ts
+++ Projeto-Stream/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SeriesView from '@/views/SeriesView/SeriesView.vue'
+import FilmesView from '../views/FilmesView.vue/FilmesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/series',
       name: 'series',
       component: SeriesView
+    },
+    {
+      path: '/filmes',
+      name: 'filmes',
+      component: FilmesView
     }
   ]
 })
