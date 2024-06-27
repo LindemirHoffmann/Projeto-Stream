f8095f57689165efddfcd3839611f963ad0a1fec
LindemirHoffmann <lindemirhoffmann10@gmail.com>
2024-06-27 15:42:26 -0300

    create view serie

--- Projeto-Stream/src/router/index.ts
+++ Projeto-Stream/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SeriesView from '@/views/SeriesView/SeriesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/series',
+      name: 'series',
+      component: SeriesView
     }
   ]
 })
---
+++ Projeto-Stream/src/views/SeriesView/SeriesView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Testando series</h1>
+</template>
