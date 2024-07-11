be55edb0986728af0fd0c2931992731ba7326d7d
Lindemir Hoffmann <108699223+LindemirHoffmann@users.noreply.github.com>
2024-07-11 14:06:47 -0300

    add primevue global (#9)

--- Projeto-Stream/src/main.ts
+++ Projeto-Stream/src/main.ts
@@ -1,14 +1,18 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import PrimeVue from 'primevue/config';
+import setPrimeVue from './modules/prime.modules'
+import setComponent from './modules/components.module'
 
+const app = createApp(App);
 app.use(createPinia())
 app.use(router)
-
+app.use(PrimeVue)
+setPrimeVue(app)
+setComponent(app)
 app.mount('#app')
