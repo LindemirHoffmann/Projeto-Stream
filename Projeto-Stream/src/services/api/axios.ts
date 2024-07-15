4f014b1b1f22891b8c852bc34900357c4b9b8209
Felipe CArneiro Ribeiro <felipecnrb@gmail.com>
2024-07-15 15:04:03 -0300

    install axios and rxjs, create folder services (#8)

---
+++ Projeto-Stream/src/services/api/axios.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError, type AxiosInstance, type AxiosRequestConfig } from "axios";
+
+//const handleError: HandleError = new HandleError();
+
+function apiConfig(baseUrl: string): AxiosRequestConfig {
+  return {
+    baseURL: baseUrl,
+  };
+}
+
+function initAxios(config: AxiosRequestConfig, token?: any): AxiosInstance {
+  const defineInstance = axios.create(config);
+  defineInstance.interceptors.request.use(
+    
+    (request:any) => {
+      console.log(request)
+      request.headers!.Authorization = token??`Bearer ${import.meta.env.d.API_TOKEN}`
+      return request;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  defineInstance.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+     // handleError.getErrors(error);
+      return Promise.reject(error);
+    }
+  );
+
+  return defineInstance;
+}
+
+function api(baseURL: string = "/baseApi", token?: any) {
+  return initAxios(apiConfig(baseURL), token);
+}
+
+export { api };
