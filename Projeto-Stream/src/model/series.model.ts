4b57ce0c6ebffe2ceddd8b5de300d66bcaa1d897
Lindemir Hoffmann <108699223+LindemirHoffmann@users.noreply.github.com>
2024-07-16 16:24:25 -0300

    create model (#11)
    changes
    
    changes and editing of the series model

---
+++ Projeto-Stream/src/model/series.model.ts
@@ -0,0 +1,18 @@
+export class Series {
+    constructor(
+        public adult?:boolean,
+        public backdrop_path?:string,
+        public genre_ids?:number[],
+        public id?:number,
+        public origin_country?:string[],
+        public original_language?:string,
+        public original_name?:string,
+        public overview?:string,
+        public popularity?:number,
+        public poster_path?:string,
+        public first_air_date?:Date,
+        public name?:string,
+        public vote_average?:number,
+        public vote_count?:number
+    ){}
+}
