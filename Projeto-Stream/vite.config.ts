53af23e8f0d214b54a79105fb2729e9867c074a0
Felipe Ribeiro <felipecnrb@gmail.com>
2024-07-22 16:49:48 -0300

    config proxy

--- Projeto-Stream/vite.config.ts
+++ Projeto-Stream/vite.config.ts
@@ -1,20 +1,32 @@
+import { defineConfig, loadEnv } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+    
+  return {
+    plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    ],
+    server: {
+      proxy: {
+        '/api': {
+         target: process.env.VITE_BASE_URL,
+         changeOrigin: true,
+         rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    resolve: {
+     alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
-})
+});
