529e857e8c36e4d6a5cbe28f2a332666675531e5
Felipe CArneiro Ribeiro <felipecnrb@gmail.com>
2024-07-29 13:40:54 -0300

    fix proxy and axios (#15)

--- Projeto-Stream/vite.config.ts
+++ Projeto-Stream/vite.config.ts
@@ -5,26 +5,24 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-    
+
+  const API_URL = `${env.VITE_API_URL ?? ''}`
+
   return {
-    plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    ],
+    plugins: [vue(), vueJsx(), vueDevTools()],
     server: {
       proxy: {
         '/api': {
-         target: process.env.VITE_BASE_URL,
-         changeOrigin: true,
-         rewrite: (path) => path.replace(/^\/api/, '')
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
     },
     resolve: {
-     alias: {
+      alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
