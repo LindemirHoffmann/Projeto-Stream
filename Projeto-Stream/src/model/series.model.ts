d397fa932ee601416c8e9ec52440b0a5c704ca7e
LindemirHoffmann <lindemirhoffmann10@gmail.com>
2024-08-05 15:47:29 -0300

    view series, services and config
    corrections

---
+++ Projeto-Stream/src/model/series.model.ts
@@ -0,0 +1,19 @@
+export class SeriesModel {
+  constructor(
+    public backdrop_path?: string,
+    public id?: number,
+    public name?: string,
+    public original_name?: string,
+    public overview?: string,
+    public poster_path?: string,
+    public media_type?: string,
+    public adult?: boolean,
+    public original_language?: string,
+    public genre_ids?: number[],
+    public popularity?: number,
+    public first_air_date?: string,
+    public vote_average?: number,
+    public vote_count?: number,
+    public origin_country?: string[]
+  ) {}
+}
